3c263a3b6d2aca952303488dc6479176392cbf03
Adeilson Silva <adeilsonsilva@dcc.ufba.br>
2015-10-13 19:47:45 -0300

    Salvando hists. para treinar SVM

---
+++ bin/run_on_videos.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+for i in {1..7}
+do
+	for j in {1..5}
+	do
+	    # C = color ; D = depth
+	    im_type="C"
+        folder_name="Real_w1"
+        session="01"	
+
+	    folder="${folder_name}/0${i}_${session}_0${j}_${im_type}.avi"
+	
+		./faceAntispoof ${folder}
+        #echo ${folder}
+        wait
+	done
+done
